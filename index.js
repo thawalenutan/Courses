5714aea5d02994aa1265d4a7cbb9c948351c562f
thawalenutan <119959626+thawalenutan@users.noreply.github.com>
2023-05-27 20:21:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+function displayAllPost(){
+    window.location.href = "../html/postslist.html";
+}
+
+function createPost() {
+    var createPost = document.getElementById('createPostModal');
+    createPost.style.display = 'block';
+}
+
